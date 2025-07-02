21954528a63df357fe456553fc19a5aa991bbfff
guver71 <gccoarite@gmail.com>
2025-07-02 18:31:02 -0500

    Consumer port

--- app.py
+++ app.py
@@ -83,4 +83,4 @@ def obtener_json():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0') # 
